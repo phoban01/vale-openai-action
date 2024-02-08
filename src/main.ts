3c3183c3ac5989f4a114d7126bfd7be276b9dd0e
Piaras Hoban <phoban01@gmail.com>
2024-02-08 19:11:55 +0000

    update prompt
    Signed-off-by: Piaras Hoban <phoban01@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -97,7 +97,7 @@ export async function run(actionInput: input.Input): Promise<void> {
           return 1;
         }
 
-        console.log(response.choices[0].message);
+        console.log(JSON.stringify(response.choices[0].message));
         const enriched: string = response.choices[0].message.content;
 
         // Pipe to reviewdog ...
